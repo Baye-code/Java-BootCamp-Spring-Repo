60e38fe775e2da21ccd689f11ec689c235991e91
Baye <bayesabarane@gmail.com>
2021-10-01 12:02:33 +0000

    adding some more projects related to JDBC ORM and REST API to the repo

---
+++ springboot-backend/src/main/java/com/bogal/springboot/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.bogal.springboot.service;
+
+import java.util.List;
+
+import com.bogal.springboot.model.Employee;
+
+
+
+public interface EmployeeService {
+	
+	Employee saveEmployee(Employee employee);
+	
+	List<Employee> getAllEmployees();
+	
+	Employee getEmployeeById(long id);
+	
+	Employee updateEmployee(Employee employee, long id);
+	
+	void deleteEmployee(long id);
+	
+	
+
+}
